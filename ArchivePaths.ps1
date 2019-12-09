344740323cbda33c2077a7114b40b906eddde84d
jensentu <57407758+jensentu@users.noreply.github.com>
2019-12-09 09:53:11 -0500

    Add files via upload

---
+++ ArchivePaths.ps1
@@ -0,0 +1,8 @@
+﻿$Path = Read-Host -Prompt 'Enter the path for the folder containing the individual user archive folders'
+
+Get-ChildItem -Path $Path -Directory | ForEach-Object {
+    $(Get-ChildItem $PSItem.FullName -Recurse -File -Filter *.pst | Select-Object -ExpandProperty FullName) -join ";"  | Select-Object -Property @{
+        Name = 'FullName'
+        Expression = {$PSItem}
+        }
+} | Export-Csv -Path "$Path\export.csv" -Encoding ASCII -NoTypeInformation
